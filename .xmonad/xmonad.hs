9f31d3a93e9f2de8446dbf65da1ba106167a014c
tnargy <clsscmstng@gmail.com>
2020-09-27 14:10:01 -0500

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,7 +1,14 @@
 import XMonad
 
+myTerm = "alacritty"
+myModMask = mod4Mask
+myBorderWidth = 2
+
 main = xmonad $ def
-    { borderWidth       = 2,
-      terminal          = "alacritty",
+    { borderWidth = 2,
+      terminal = myTerm,
+      ModMask = myModMask,
+      borderWidth = myBorderWidth,
       normalBorderColor = "#cccccc",
-      focusedBorderColor  = "#cd8b00" }
+      focusedBorderColor = "#cd8b00" }
+
